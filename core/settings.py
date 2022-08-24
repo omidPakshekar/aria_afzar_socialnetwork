2425df3e581e72845bc553530f39b6747f1172aa
omid.pakshekar.est@gmail.com <omid.pakshekar.est@gmail.com>
2022-08-24 19:16:35 +0430

    add chat

--- core/settings.py
+++ core/settings.py
@@ -53,6 +53,7 @@ class Dev(Configuration):
         'homepage',
         'payment',
         'posts',
+        'chat',
         
         # third package party
         'allauth',
@@ -119,8 +120,17 @@ class Dev(Configuration):
         },
     ]
 
-    WSGI_APPLICATION = 'core.wsgi.application'
-
+    # WSGI_APPLICATION = 'core.wsgi.application'
+    # WSGI_APPLICATION = 'core.wsgi.application'
+    ASGI_APPLICATION = 'core.asgi.application'
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            'CONFIG': {
+                "hosts": [('127.0.0.1', 6379)],
+            },
+        },
+    }
 
     # Database
     # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
