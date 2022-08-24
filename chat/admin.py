15327ac1b3e206b92701b73638e37054dce88b10
omid.pak.developer <omid.pak.developer@gmail.com>
2022-08-24 18:10:26 +0430

    fix model bug and model admin

--- chat/admin.py
+++ chat/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Message)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'list_5_first_friends']
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'message_text', 'parent_message', 'timestamp']
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ['id',]
+
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(Contact, ContactAdmin)
+
+
+
--- chat/models.py
+++ chat/models.py
@@ -1,28 +1,36 @@
+from cgitb import text
+from pyexpat import model
 from django.db import models
 from users.models import CustomeUserModel
 
 class Contact(models.Model):
-    owner = models.ForeignKey(CustomeUserModel, related_name='friends', on_delete=models.CASCADE)
-    friends = models.ManyToManyField('self', blank=True)
+    owner       = models.ForeignKey(CustomeUserModel, related_name='contacts', on_delete=models.CASCADE)
+    friends     = models.ManyToManyField(CustomeUserModel, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.owner.username
+    
+    @property
+    def list_5_first_friends(self):
+        frinds =  self.friends.all()[0:5]
+        return ' '.join([str(i) for i in frinds])
 
 class Message(models.Model):
-    owner = models.ForeignKey(CustomeUserModel, related_name='chat_messages', on_delete=models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    owner          = models.ForeignKey(CustomeUserModel, related_name='chat_messages', on_delete=models.CASCADE)
+    parent_message = models.OneToOneField('Message', related_name='child', null=True,
+                          on_delete=models.SET_NULL, blank=True)
+    message_text   = models.TextField()
+    timestamp      = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.author.username
-
-    def last_10_messages():
-        return Message.objects.order_by('-timestamp').all()[:10]
+        return self.contact.owner.username
 
 class Chat(models.Model):
-    participants = models.ManyToManyField(Contact, related_name='chats', blank=True)
-    messages = models.ManyToManyField(Message, blank=True)
+    participants = models.ForeignKey(Contact, related_name='chats', blank=True, on_delete=models.CASCADE)
+    messages     = models.ManyToManyField(Message, blank=True)
 
+    def last_10_messages(self):
+        return self.messages.objects.order_by('-timestamp')
     def __str__(self):
         return "{}".format(self.pk)
 
