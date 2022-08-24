7d8e83d6e5a760f11b33919df3130ed965e3c10f
omid.pak.developer <omid.pak.developer@gmail.com>
2022-08-24 16:06:03 +0430

    add chat and contact model

--- chat/models.py
+++ chat/models.py
@@ -1,10 +1,15 @@
-from django.contrib.auth import get_user_model
 from django.db import models
+from users.models import CustomeUserModel
 
-User = get_user_model()
+class Contact(models.Model):
+    owner = models.ForeignKey(CustomeUserModel, related_name='friends', on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 class Message(models.Model):
-    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+    owner = models.ForeignKey(CustomeUserModel, related_name='chat_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -13,3 +18,13 @@ class Message(models.Model):
 
     def last_10_messages():
         return Message.objects.order_by('-timestamp').all()[:10]
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(Contact, related_name='chats', blank=True)
+    messages = models.ManyToManyField(Message, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.pk)
+
+
+
